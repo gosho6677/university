e84fee45ee2410c53822eaab5b435bd56bb958ae
gosho6677 <59176653+gosho6677@users.noreply.github.com>
2021-12-27 19:31:06 +0200

    Add remaining view templates

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # university
+- [x] Add Frontend view templates
+- [ ] Create the database, tables and its relations
+- [ ] Update models...
--- controllers/recordsController.js
+++ controllers/recordsController.js
@@ -16,4 +16,24 @@ router.get('/create/subject', (req, res) => {
     res.render('createSubject');
 });
 
+router.get('/students-and-disciplines', (req, res) => {
+    res.render('studentsAndDisciplines');
+});
+
+router.get('/students-and-credits', (req, res) => {
+    res.render('studentsAndCredits');
+});
+
+router.get('/teachers-and-subjects', (req, res) => {
+    res.render('teachersAndSubjects');
+});
+
+router.get('/most-enrolled-subjects', (req, res) => {
+    res.render('mostEnrolledSubjects');
+});
+
+router.get('/top-three-teachers', (req, res) => {
+    res.render('topThreeTeachers');
+});
+
 module.exports = router;
